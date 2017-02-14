93842b8fb30fd2f245e8fd4aff5b7683feb108a4
luxz <luxz@51idc.com>
2017-02-14 15:03:55 +0800

    外层增加try catch 捕获异常并打印

--- zipkin-collector/kafka/src/main/java/zipkin/collector/kafka/KafkaStreamProcessor.java
+++ zipkin-collector/kafka/src/main/java/zipkin/collector/kafka/KafkaStreamProcessor.java
@@ -13,10 +13,17 @@
  */
 package zipkin.collector.kafka;
 
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.List;
+
+import com.google.gson.annotations.JsonAdapter;
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import zipkin.Codec;
+import zipkin.Span;
 import zipkin.collector.Collector;
 import zipkin.collector.CollectorMetrics;
 
@@ -27,6 +34,7 @@ final class KafkaStreamProcessor implements Runnable {
   final KafkaStream<byte[], byte[]> stream;
   final Collector collector;
   final CollectorMetrics metrics;
+  final Logger logger = LoggerFactory.getLogger(KafkaStreamProcessor.class);
 
   KafkaStreamProcessor(
       KafkaStream<byte[], byte[]> stream, Collector collector, CollectorMetrics metrics) {
@@ -41,11 +49,27 @@ final class KafkaStreamProcessor implements Runnable {
     while (messages.hasNext()) {
       byte[] bytes = messages.next().message();
       metrics.incrementMessages();
+      logger.info("new message comming from kafka," + new String(bytes));
 
-      if (bytes.length == 0) {
-        metrics.incrementMessagesDropped();
+      if (bytes == null) {
+        logger.info("message is null.");
         continue;
       }
+      if (bytes.length == 0) {
+        metrics.incrementMessagesDropped();
+        logger.info("message is empty.");
+        continue;
+      }
+//      logger.info("start read spans.");
+//      try {
+//        List<Span> spans = Codec.THRIFT.readSpans(bytes);
+//        logger.info("span list count: " + spans.size());
+//        for (Span span : spans) {
+//          logger.info("id:" + span.id + ", name:" + span.name + ", traceId:" + span.traceId);
+//        }
+//      }catch (Exception e){
+//        logger.error(e.getMessage(),e);
+//      }
 
       // In TBinaryProtocol encoding, the first byte is the TType, in a range 0-16
       // .. If the first byte isn't in that range, it isn't a thrift.
@@ -56,14 +80,19 @@ final class KafkaStreamProcessor implements Runnable {
       // .. When serializing a Span (Struct), the first byte will be the type of a field
       // .. When serializing a List[ThriftSpan], the first byte is the member type, TType.STRUCT(12)
       // .. As ThriftSpan has no STRUCT fields: so, if the first byte is TType.STRUCT(12), it is a list.
-      if (bytes[0] == '[') {
-        collector.acceptSpans(bytes, Codec.JSON, NOOP);
-      } else {
-        if (bytes[0] == 12 /* TType.STRUCT */) {
-          collector.acceptSpans(bytes, Codec.THRIFT, NOOP);
+      try {
+        if (bytes[0] == '[') {
+          collector.acceptSpans(bytes, Codec.JSON, NOOP);
         } else {
-          collector.acceptSpans(Collections.singletonList(bytes), Codec.THRIFT, NOOP);
+          if (bytes[0] == 12 /* TType.STRUCT */) {
+            collector.acceptSpans(bytes, Codec.THRIFT, NOOP);
+          } else {
+            collector.acceptSpans(Collections.singletonList(bytes), Codec.THRIFT, NOOP);
+          }
         }
+        logger.info("message process down, has next: " + messages.hasNext());
+      }catch (Exception e){
+        logger.error(e.getMessage(),e);
       }
     }
   }
